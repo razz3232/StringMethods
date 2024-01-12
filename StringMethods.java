9d472a31541bb1a7aa484ff0353746a41dd50e0a
razz3232 <74662119+razz3232@users.noreply.github.com>
2024-01-12 01:40:15 +0000

    output done

--- StringMethods.java
+++ StringMethods.java
@@ -1,1 +1,18 @@
+/**
+ * StringMethods
+ */
+public class StringMethods {
+    public static void main(String[] args) {
+        String name = "javatraining";
+        System.out.println(name.length());
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.substring(0, 1));
+        System.out.println(name.substring(11, 12));
+        System.out.println(name.substring(1, 5));
 
+
+    }
+
+    
+}
