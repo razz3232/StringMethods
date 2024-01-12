935e6b7bb20be22ed75cf5a367fecc117ca8e58e
razz3232 <74662119+razz3232@users.noreply.github.com>
2024-01-12 05:05:18 +0000

    done

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -3,12 +3,22 @@ import java.util.Scanner;
 
 class Palindrome {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        /*Scanner input = new Scanner(System.in);
         System.out.println("Enter a word");
         String palin = input.nextLine();
         StringBuilder palinrev = new StringBuilder(palin);
         String message = (palin.equalsIgnoreCase(palinrev.reverse().toString()))? "Palindrome": "not palindrome";
-        System.out.println(message);
+        System.out.println(message);*/
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a word");
+        String word = input.nextLine();
+        StringBuilder sb = new StringBuilder(word);
+        sb.reverse();
+        if(sb.equals(sb.reverse()))
+        System.out.println("Palindrome");
+        System.out.println("not palindrome");
+
 
     }
 }
