28118f5853ff5a8df3348807931e3c4cc7cdd338
razz3232 <74662119+razz3232@users.noreply.github.com>
2024-01-12 02:35:17 +0000

    output done

--- StringMethods.java
+++ StringMethods.java
@@ -7,8 +7,11 @@ public class StringMethods {
         System.out.println(name.length());
         System.out.println(name.toUpperCase());
         System.out.println(name.toLowerCase());
-        System.out.println(name.substring(0, 1));
-        System.out.println(name.substring(11, 12));
+        System.out.println(name.charAt(0));
+        System.out.println(name.charAt(11));
+
+       // System.out.println(name.substring(0, 1));
+      // System.out.println(name.substring(11, 12));
         System.out.println(name.substring(1, 5));
 
 
